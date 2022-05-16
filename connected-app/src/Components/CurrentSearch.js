af28ef8e45aa4842e00f36fd67cb9b7ffa8a8763
Nicholas Meade <nicholasjmeade@hotmail.com>
2022-05-16 13:28:39 -0400

    work on currentsearch.js component 1. established structure for currentsearch.js component 2. placeholders added - to flesh out functionality once starting to work on API call 3. light css added to remove bullet points from list items

--- connected-app/src/Components/CurrentSearch.js
+++ connected-app/src/Components/CurrentSearch.js
@@ -1,7 +1,26 @@
+import './Components.css';
+
 const CurrentSearch = () => {
     return ( 
-        <p>Current Search Holder</p>
+        <div className="currentsearch-container">
+            <div className="user-current-weather">
+                <h2>Your weather today in (user location) is:</h2>
+            </div>
+            <div className="weather-data">
+                <ul>
+                    <li>Current Temperature</li>
+                    <li>High Temperature</li>
+                    <li>Low Temperature</li>
+                </ul>
+            </div>
+            <div className="user-search-calltoaction">
+                <h2>Be Connected - search anywhere in the world to see what the weather is!</h2>
+                <form>
+                    <input type="text"></input>
+                </form>
+            </div>
+        </div>
      );
 }
- 
+
 export default CurrentSearch;
