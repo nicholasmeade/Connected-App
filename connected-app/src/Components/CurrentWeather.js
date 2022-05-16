2b548aefbc28446468fd09132841ebd77950b3f4
Nicholas Meade <nicholasjmeade@hotmail.com>
2022-05-16 13:37:53 -0400

    starting to work on APIs 1. did some research and found out how to auto detect a user's time zone 2. more search now into moment.js to be able to display the user's local time

--- connected-app/src/Components/CurrentWeather.js
+++ connected-app/src/Components/CurrentWeather.js
@@ -2,11 +2,14 @@ import CurrentSearch from "./CurrentSearch";
 import {Routes, Route, Link, Navigate} from "react-router-dom";
 
 const CurrentWeather = () => {
+    const date = new Date();
+    const offset = date.getTimezoneOffset();
+    console.log(offset)
     return ( 
         <div className="current-container">
             <div className="user-greeting">
                 <h1>Good (morning/afternoon/evening).</h1>
-                <h2>It is currently (xx:xx) in (user location).</h2>
+                <h2>It is currently {offset} in (user location).</h2>
             </div>
             <div className="app-intro">
                 <p>Welcome to Connected - a small space on the internet to feel more connected with all parts of the world, both now and in the past.</p>
